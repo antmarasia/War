9dbcde4f567bb7bf49a3c217fdda0e96eb1e873a
antmarasia <antmarasia@users.noreply.github.com>
2021-04-27 15:30:56 -0400

    Add game logic

---
+++ README.md
@@ -0,0 +1,14 @@
+# War
+
+Simple war game made with SwiftUI
+
+<img src="./screenshots/start.png" width=300 align=left>
+<img src="./screenshots/draw_2.png" width=300 align=right>
+
+## Requirements
+
+iOS 14+
+
+Tested with Xcode 12 and Swift 5
+
+
--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -8,9 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "back"
+    @State var cpuCard = "back"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button(action: {
+                    
+                    // Generate a random number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    // Update the cards
+                    playerCard = "card\(playerRand)"
+                    cpuCard = "card\(cpuRand)"
+                    
+                    // Update the score
+                    
+                    if cpuRand == playerRand {
+                        playerScore += 1
+                        cpuScore += 1
+                        return
+                    }
+                    
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    } else {
+                        cpuScore += 1
+                    }
+                    
+                    
+                }, label: {
+                    Image("dealbutton")
+                        .renderingMode(.original)
+                })
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                        Text("\(playerScore)")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                        Text("\(cpuScore)")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+        
     }
 }
 
